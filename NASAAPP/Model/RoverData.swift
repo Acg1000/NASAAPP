5540c6b50997e71f5dda71e57a5433384e60c7cc
andrewgraves(MAC) <andrewcgraves@gmail.com>
2020-01-02 14:35:29 -0800

    Initial Commit

---
+++ NASAAPP/Model/RoverData.swift
@@ -0,0 +1,24 @@
+//
+//  RoverData.swift
+//  NASAAPP
+//
+//  Created by Andrew Graves on 1/2/20.
+//  Copyright © 2020 Andrew Graves. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct RoverData {
+    let id: Int
+    let imageURL: URL
+    let earthDate: String
+    let roverName: String
+    
+}
+
+//extension RoverData {
+//    var image: UIImage = {
+//
+//    }
+//}
---
+++ NASAAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NASAAPP
+//
+//  Created by Andrew Graves on 1/2/20.
+//  Copyright © 2020 Andrew Graves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
